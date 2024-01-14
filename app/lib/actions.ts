865ea908be93b3336b3794b821a679fba96dc5fc
tsunose <tsunose.developer@gmail.com>
2024-01-14 23:22:16 +0900

    mutating data

---
+++ app/lib/actions.ts
@@ -0,0 +1,79 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // TODO: このパスだったらサーバに取得し直すという命令？後でちゃんと理解する
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+
+  //
+  // NOTE: console.logの結果の見え方
+  // console.log(rawFormData);
+  // console.log("----");
+  // console.log(Object.fromEntries(formData.entries()));
+  // {
+  //   customerId: 'cc27c14a-0acf-4f4a-a6c9-d45682c144b9',
+  //   amount: '1',
+  //   status: 'pending'
+  // }
+  // ----
+  // {
+  //   '$ACTION_ID_35a81b6073a1817ee01c76a4075314616ca38aed': '',
+  //   customerId: 'cc27c14a-0acf-4f4a-a6c9-d45682c144b9',
+  //   amount: '1',
+  //   status: 'pending'
+  // }
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
